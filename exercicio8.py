7f3ae639c5c905196e211af4820fddbbbf7954b9
letyresina <leticiaresina@gmail.com>
2023-05-02 08:25:56 -0300

    Correção no exercício 8

--- exercicio8.py
+++ exercicio8.py
@@ -19,6 +19,9 @@
     o programa com a opção 5
 '''
 
+from optparse import OptionContainer
+
+
 def menuOpcoes():
     print("Bem vindo(a) à Calculadora Python! \n Escolha uma opção abaixo:")
     print("1 - Adição \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 5 - Sair do programa")
@@ -40,27 +43,30 @@ def funcaoMultiplicacao(num1, num2):
 def funcaoDivisao(num1, num2):
     divisao = num1 / num2
     print(divisao)
-
-# Perguntar pro professor porque pede pra repetir a opção mais de uma vez! 
+ 
 while True:
-    menuOpcoes()
-    if menuOpcoes() == 1:
+    '''
+        Armazeno o valor da função dentro de uma variável, pois, se eu chamar a função em cada
+        condição, o programa vai executar todas as vezes até confirmar qual condição é a correta!
+    '''
+    opcao = menuOpcoes()
+    if opcao == 1:
         num1 = float(input("Informe o primeiro número: "))
         num2 = float(input("Informe o segundo número: "))
         funcaoAdicao(num1, num2)
-    elif menuOpcoes() == 2:
+    elif opcao == 2:
         num1 = float(input("Informe o primeiro número: "))
         num2 = float(input("Informe o segundo número: "))
         funcaoSubtracao(num1, num2)
-    elif menuOpcoes() == 3:
+    elif opcao == 3:
         num1 = float(input("Informe o primeiro número: "))
         num2 = float(input("Informe o segundo número: "))
         funcaoMultiplicacao(num1, num2)
-    elif menuOpcoes() == 4:
+    elif opcao == 4:
         num1 = float(input("Informe o primeiro número: "))
         num2 = float(input("Informe o segundo número: "))
         funcaoDivisao(num1, num2)
-    elif menuOpcoes() == 5:
+    elif opcao == 5:
         print("Finalizando a calculadora...")
         break
     else:
